3de3702cac26c6870978acad273b9dc075fb2aa3
dream19 <supattra@bananacoding.com>
2021-08-05 16:31:20 +0700

    change name project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-crud-example app is running!');
+    expect(await page.getTitleText()).toEqual('demo-employee-angular app is running!');
   });
 
   afterEach(async () => {
